dd5c0ebb99a41737673c069bb055d0b4cfe4b69a
CalculatedMatters <hello@calculatedmatters.com.au>
2025-10-17 10:25:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,10 @@ CURATED_DEFAULT_FEEDS = [
     "https://www.theguardian.com/music/australian-music/rss",
     "https://australianmusician.com.au/feed/",
     "https://www.noise11.com/feed/",
+    "https://www.cutcommonmag.com/feed/",
     "https://au.rollingstone.com/feed/",
+    "https://au.rollingstone.com/music/feed/",
+    "https://thehordern.com.au/feed/",
 ]
 
 DEFAULT_CLIENTS = [
@@ -560,3 +563,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
